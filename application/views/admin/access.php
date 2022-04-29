527bdb33f6f9cd483a98d45500f6d3b3926863d7
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2022-04-29 22:19:22 +0600

    Update UI

--- application/views/admin/access.php
+++ application/views/admin/access.php
@@ -25,7 +25,7 @@
 
             <form action="<?= site_url('access_auth') ?>" method="post">
                 <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
